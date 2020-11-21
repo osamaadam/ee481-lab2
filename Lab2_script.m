5b55dc8fc052a9dbbbaa78ad44487998af2da571
Osama Adam <osamaadam98@gmail.com>
2020-11-21 20:54:02 +0200

    calculating No properly

--- Lab2_script.m
+++ Lab2_script.m
@@ -53,6 +53,7 @@ title('A square pulse in time and frequency domains','linewidth',10)
 
 % See the effect of an AWGN channel for a particular Eb/No value
 Eb_No_db = 0;       % The specified Eb/No value in dB
+No = calculateNo(Eb_No_db, Energy_per_bit);
 
 %%% WRITE YOUR CODE HERE
 % Knowing the value of Eb/No in dB, and for the given energy per bit value
@@ -62,7 +63,7 @@ Eb_No_db = 0;       % The specified Eb/No value in dB
 %%%
 
 %%% Implement the effect of the AWGN channel
-No = 2;
+
 y_square = AWGNChannel(x_square,No,fs);     % IMPLEMENT THIS: the output of the AWGN channel should be given in y_square.
 
 figure
@@ -311,14 +312,15 @@ BER_uni = zeros(size(Eb_No_dB_vector));
 x_bits = randi([0, 1], [1, N_bits]);
 
 for i = 1 : length(Eb_No_dB_vector)
+  No = calculateNo(Eb_No_dB_vector(i), Energy_per_bit);
   x_square = GenerateSquarePulses(t_axis, T_sq, Energy_per_bit, fs, x_bits, 'unipolar'); 
-  y_square = AWGNChannel(x_square, i, fs);
+  y_square = AWGNChannel(x_square, Eb_No_dB_vector(i), fs);
   [rec_bits, ht, z_square, samples] = MatchedFilter(T_sq, Energy_per_bit, fs, y_square, 'unipolar'); 
   
   BER_uni(i) = sum(bitxor(x_bits, samples(1: N_bits))) / N_bits;
   
   x_square = GenerateSquarePulses(t_axis, T_sq, Energy_per_bit, fs, x_bits, 'bipolar'); 
-  y_square = AWGNChannel(x_square, i, fs);
+  y_square = AWGNChannel(x_square, Eb_No_dB_vector(i), fs);
   [rec_bits, ht, z_square, samples] = MatchedFilter(T_sq, Energy_per_bit, fs, y_square, 'bipolar'); 
   
   BER_bi(i) = sum(bitxor(x_bits, samples(1: N_bits))) / N_bits;
---
+++ calculateNo.m
@@ -0,0 +1,3 @@
+function No = calculateNo(SNR, Eb);
+  No = Eb / (10 ^ (SNR / 10)); 
+end
